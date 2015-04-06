68351d5cc44dba071fe4b073fb35141a0fdbdf6e
paldepind <simonfv@gmail.com>
2015-04-06 11:29:23 +0200

    Add `stream`

--- obj.js
+++ obj.js
@@ -1,21 +1,43 @@
 var flyd = require('flyd');
 
-exports.toStreams = function(obj) {
+function wrapProps(from, to) {
+  for (var key in from) {
+    if (from.hasOwnProperty(key)) {
+      to[key] = flyd.stream(from[key]);
+    }
+  }
+  return to;
+}
+
+exports.streamProps = function(obj) {
+  return wrapProps(obj, {});
+};
+
+exports.extractProps = function extract(obj) {
   var newObj = {};
   for (var key in obj) {
     if (obj.hasOwnProperty(key)) {
-      newObj[key] = flyd.stream(obj[key]);
+      newObj[key] = flyd.isStream(obj[key]) ? obj[key].val : obj[key];
     }
   }
   return newObj;
 };
 
-exports.extract = function(obj) {
-  var newObj = {};
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      newObj[key] = 'val' in obj[key] ? obj[key].val : obj[key];
-    }
-  }
-  return newObj;
+function update(s, keys) {
+  var obj = {};
+  keys.map(function(key) {
+    obj[key] = s[key].val;
+  });
+  s(obj);
+}
+
+exports.stream = function(obj) {
+  var s = flyd.stream();
+  wrapProps(obj, s);
+  var keys = Object.keys(obj);
+  var upd = update.bind(null, s, keys);
+  keys.map(function(key) {
+    flyd.map(upd, s[key]);
+  });
+  return s;
 };
--- test/index.js
+++ test/index.js
@@ -4,19 +4,56 @@ var stream = flyd.stream;
 
 var obj = require('../obj.js');
 
-describe('to streams', function() {
+describe('stream props', function() {
   it('converts the properties in an object to streams', function() {
     var o = {one: 1, two: 2, three: 3};
-    var oS = obj.toStreams(o);
+    var oS = obj.streamProps(o);
     assert.equal(o.one, oS.one());
     assert.equal(o.two, oS.two());
     assert.equal(o.three, oS.three());
   });
+});
+describe('stream object', function() {
+  var o = {one: 1, two: 2, three: 3};
+  it('converts properties to streams', function() {
+    var oS = obj.stream(o);
+    assert.equal(o.one, oS.one());
+    assert.equal(o.two, oS.two());
+    assert.equal(o.three, oS.three());
+  });
+  it('returns a stream', function() {
+    var oS = obj.stream(o);
+    assert(flyd.isStream(oS));
+  });
+  it('flow unwrapped object down stream when props change', function() {
+    var oS = obj.stream(o);
+    var result = [];
+    flyd.map(function(o) { result.push(o); }, oS);
+    oS.one(4);
+    oS.three(4);
+    oS.two(4);
+    assert.deepEqual(result, [
+      {one: 1, two: 2, three: 3},
+      {one: 4, two: 2, three: 3},
+      {one: 4, two: 2, three: 4},
+      {one: 4, two: 4, three: 4},
+    ]);
+  });
+});
+describe('extract', function() {
   it('extracts the values from streams in object', function() {
     var oS = {one: stream(1), two: stream(2), three: stream(3)};
     var o = obj.extract(oS);
     assert.equal(o.one, oS.one());
     assert.equal(o.two, oS.two());
     assert.equal(o.three, oS.three());
+  });
+  it('handles values that are not streams', function() {
+    var oS = {one: stream(1), undef: undefined, nll: null, two: 2};
+    var o = obj.extract(oS);
+    assert.equal(o.one, oS.one());
+    assert.equal(o.undef, oS.undef);
+    assert.equal(o.nll, oS.nll);
+    assert.equal(o.two, oS.two);
   });
 });
