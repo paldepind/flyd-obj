cfa4188b62b0636a2956eac52d94e398a20ca107
Adam Nalisnick <theadam4257@gmail.com>
2015-04-29 23:24:22 -0400

    Added support for nested objects

--- obj.js
+++ obj.js
@@ -1,28 +1,44 @@
 var flyd = require('flyd');
 
-exports.streamProps = function(from) {
+function isPlainObject(obj) {
+  return obj !== null && typeof obj === 'object' && Object.getPrototypeOf(obj) === Object.prototype;
+}
+
+var streamProps = function(from) {
   var to = {};
   for (var key in from) {
     if (from.hasOwnProperty(key)) {
-      to[key] = flyd.stream(from[key]);
+      to[key] = isPlainObject(from[key]) ? streamProps(from[key]) : flyd.stream(from[key]);
     }
   }
   return to;
 };
 
-var extractProps = exports.extractProps = function(obj) {
+var extractProps = function(obj) {
   var newObj = {};
   for (var key in obj) {
     if (obj.hasOwnProperty(key)) {
-      newObj[key] = flyd.isStream(obj[key]) ? obj[key]() : obj[key];
+      newObj[key] = isPlainObject(obj[key]) ? extractProps(obj[key])
+                  : flyd.isStream(obj[key]) ? obj[key]()
+                                            : obj[key];
     }
   }
   return newObj;
 };
 
-exports.stream = function(obj) {
-  var streams = Object.keys(obj).map(function(key) { return obj[key]; });
+var stream = function(obj) {
+  var streams = Object.keys(obj).map(function(key) {
+    return isPlainObject(obj[key]) ? stream(obj[key])
+         : flyd.isStream(obj[key]) ? obj[key]
+                                   : flyd.stream(obj[key]);
+  });
   return flyd.stream(streams, function() {
     return extractProps(obj);
   });
+};
+
+module.exports = {
+  streamProps: streamProps,
+  extractProps: extractProps,
+  stream: stream
 };
